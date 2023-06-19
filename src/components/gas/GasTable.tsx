915df5198e8d19f32e51eea4369563e0d8db5b3a
Maria Roestorff <maria.roestorff@gmail.com>
2023-06-19 11:24:21 +0100

    removed but still in useeffect

--- src/components/gas/GasTable.tsx
+++ src/components/gas/GasTable.tsx
@@ -98,20 +98,18 @@ export default function GasTable({
       {displayData?.map(({ gasLogID, topup, units, measuredAt, rate }) => {
         if (editID !== 0 && editID === gasLogID) {
           return (
-            <div key={`tablerow-${gasLogID}-editrow`}>
-              <div>{JSON.stringify(utilsInputx)}</div>
-              <EditRow
-                logID={gasLogID}
-                urlPath="gas"
-                // units={units}
-                // topup={topup}
-                utilsInputx={utilsInputx}
-                setUtilsInputx={setUtilsInputx}
-                measuredAt={measuredAt}
-                handleCancel={handleCancel}
-                triggerDataRefresh={triggerDataRefresh}
-              />
-            </div>
+            <EditRow
+              key={`tablerow-${gasLogID}-editrow`}
+              logID={gasLogID}
+              urlPath="gas"
+              // units={units}
+              // topup={topup}
+              utilsInputx={utilsInputx}
+              setUtilsInputx={setUtilsInputx}
+              measuredAt={measuredAt}
+              handleCancel={handleCancel}
+              triggerDataRefresh={triggerDataRefresh}
+            />
           );
         }
         return (
